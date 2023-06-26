8c21e4cb59396af8d3fd7ab16313eb610542c9bf
logan-lampton <lampton.logan@gmail.com>
2023-06-26 19:31:45 -0400

    u.s. states quiz created

---
+++ states-quiz/main.py
@@ -0,0 +1,55 @@
+from turtle import Turtle, Screen
+import pandas as pd
+import string
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+states = Turtle()
+screen = Screen()
+screen.title("Guess the U.S. States!")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+states.shape(image)
+
+# JUST FYI: how to track mouse clicks to grab state coordinates
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(fun=get_mouse_click_coor)
+
+data = pd.read_csv("50_states.csv")
+
+correct = 0
+guessed_states = []
+playing = True
+
+while playing:
+    user_answer = string.capwords(screen.textinput(title=f"{correct}/50 states correct",
+                                                   prompt="What's another state name?"))
+
+    if user_answer == "Exit":
+        missing_states = []
+        for state in data.state:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states.learn.csv")
+        break
+    for state in data.state:
+        if state == user_answer:
+            guessed_states.append(user_answer)
+            correct_state = data[data.state == f"{user_answer}"]
+            text = Turtle()
+            text.penup()
+            text.hideturtle()
+            text.goto(float(correct_state.x - 15), float(correct_state.y - 5))
+            text.write(f"{user_answer}")
+            correct += 1
+
+        if correct == 50:
+            playing = False
+            you_win = Turtle()
+            you_win.pen()
+            you_win.hideturtle()
+            you_win.write("You win, smartypants!")
