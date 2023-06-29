bc64f6cd1227e5d2f0e4ca872da91b271b01330c
logan-lampton <lampton.logan@gmail.com>
2023-06-29 11:49:13 -0400

    updated timer ui

--- pomodoro-timer/main.py
+++ pomodoro-timer/main.py
@@ -25,7 +25,18 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 45, "bold"))
+timer_label.grid(column=1, row=0)
+
+checkmark_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+checkmark_label.grid(column=1, row=3)
+
+start_btn = Button(text="Start", font=(FONT_NAME, 10), highlightthickness=0)
+start_btn.grid(column=0, row=2)
+
+reset_btn = Button(text="Reset", font=(FONT_NAME, 10), highlightthickness=0)
+reset_btn.grid(column=2, row=2)
 
 window.mainloop()
