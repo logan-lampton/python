1a62457342c9ac3796e16fb13c70d016ea5a68b9
logan-lampton <lampton.logan@gmail.com>
2023-07-06 12:27:54 -0400

    added functionality to create new flashcards using csv data

--- language-flash-cards/main.py
+++ language-flash-cards/main.py
@@ -1,7 +1,25 @@
+import random
 from tkinter import *
+import pandas as pd
+from random import *
+
+# ---------------------------- DATA FOR FLASH CARDS ------------------------------- #
+
+data = pd.read_csv("data/french_words.csv")
+word_data = data.to_dict(orient="records")
+initial_choice = choice(word_data)
+initial_word = initial_choice["French"]
+current_language = "French"
+
 
 # ---------------------------- CREATE NEW FLASH CARDS ------------------------------- #
 
+
+def next_card():
+    current_card = choice(word_data)
+    while current_card == initial_choice:
+        current_card = choice(word_data)
+    word_label.config(text=current_card["French"])
 
 
 # ---------------------------- UI SETUP --------------------------------------------- #
@@ -20,19 +38,19 @@ card_front = PhotoImage(file="images/card_front.png")
 front_card = Label(frame, image=card_front, background=BACKGROUND_COLOR)
 front_card.grid(column=0, row=0)
 
-language_label = Label(frame, text="Language", background="white", font=("Ariel", 40, "italic"))
-language_label.place(x=300, y=100)
+language_label = Label(frame, text=current_language, background="white", font=("Ariel", 40, "italic"))
+language_label.place(x=250, y=100)
 
-word_label = Label(frame, text="Word", background="white", font=("Ariel", 60, "bold"))
-word_label.place(x=300, y=223)
+word_label = Label(frame, text=initial_word, background="white", font=("Ariel", 60, "bold"))
+word_label.place(x=250, y=223)
 
 # Buttons
 wrong_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 checkmark_img = PhotoImage(file="images/right.png")
-right_button = Button(image=checkmark_img, highlightthickness=0)
+right_button = Button(image=checkmark_img, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 
 
